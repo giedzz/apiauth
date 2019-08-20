f48fa6cca54f0e756306f032060eef43c974d957
Giedrius S <kaliosaz@gmail.com>
2019-08-20 15:11:56 +0300

    Toliau dirbama su front-end, pridėta registracijos forma ir perdavimas back-end'ui

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,28 @@
+import { AUTH_SIGN_UP, AUTH_ERROR } from '../actions/types';
+
+const DEFAULT_STATE = {
+    isAuthenticated: false,
+    token: '',
+    errorMessage: ''
+}
+
+export default (state = DEFAULT_STATE, action) => {
+    switch(action.type){
+        case AUTH_SIGN_UP:
+                console.log('[ActionCreator] got an AUTH_SIGN_UP action!');
+            return {
+                ...state,
+                token: action.payload,
+                isAuthenticated: true,
+                errorMessage: ''
+            }
+        case AUTH_ERROR:
+                console.log('[ActionCreator] got an AUTH_ERROR action!');
+            return {
+                ...state,
+                errorMessage: action.payload
+            }
+        default:
+        return state;
+    }
+}
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 if (!process.env.NODE_ENV === 'test'){
@@ -13,6 +14,8 @@ if (!process.env.NODE_ENV === 'test'){
 
 
 const app = express();
+
+app.use(cors());
 
 // Middlewares 
 if (!process.env.NODE_ENV === 'test'){
